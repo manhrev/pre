acd4a0fde48f125d217bb68f6f359cb7509b0e43
Manh Nguyen <manhnd@inspirelab.io>
2022-07-08 13:24:39 +0700

    update

--- main.go
+++ main.go
@@ -39,13 +39,14 @@ func main() {
 	for _ = range time.Tick(20 * time.Millisecond) {
 		if id%30 == 0 {
 
-			world.NewPlayerAt(id, 550, 350)
+			world.NewPlayerAt(id, 400, 350)
 			world.Players[id].SetFacing((rand.Float64()) * 2 * 3.1415)
-			world.Players[id].SetVelocity(rand.Float64()*5 + 1)
+			world.Players[id].SetVelocity(rand.Float64()*10 + 1)
 			//world.Players[id].SetVelocity(0)
 		}
 		id++
 		objManager.UpdateObjects()
+		collisionManager.ResolveCollisionsOnMap()
 		collisionManager.ResolveCollisions()
 		collisionManager.ResolveCollisionsOnMap()
 
